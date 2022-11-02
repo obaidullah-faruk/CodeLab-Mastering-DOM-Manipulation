e3447b976eeac71d2e2cab212eb29803dde51e72
obaidullah.faruk <obaidullah.faruk@brotecs.com>
2022-11-02 11:22:38 +0600

    selecting element in the dom tree

---
+++ selecting_element_in_the_DOM/app.js
@@ -0,0 +1,24 @@
+// DOM Manipulation 
+
+
+// GetElementById()
+const title = document.getElementById9('main-heading');
+console.log(title)
+
+
+// GetElementByClassName()
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+
+// GetElementByTagName()
+const tagItem = document.getElementsByTagName('li');
+console.log(tagItem);
+
+// querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+// querySelectorAll()
+const containerAll = document.querySelectorAll('div');
+console.log(containerAll);
